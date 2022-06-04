1084d97e67f995b46d8fa740adfe929e81ec32c7
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-04 03:59:42 -0500

    added treepeat.sh script

---
+++ notes/treepeat.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+  echo "usage: treepeat <source> <output>"
+  exit 1
+fi
+
+WORKDIR=$PWD
+SOURCEDIR=$1
+OUTPUT=$2
+
+echo "using $1 as source and copying directory structure into ../fastQC_reports"
+echo "creating directory for fastQC output"
+cd $WORKDIR &&  mkdir $OUTPUT/
+TARGETDIR="$OUTPUT/"
+echo "copying directory structure of $METADIR into $TARGETDIR"
+tree -dfi --noreport $SOURCEDIR | xargs -I{} mkdir -p "$TARGETDIR/{}"
+echo "treePeat done!"
+tree -dfi $TARGETDIR
