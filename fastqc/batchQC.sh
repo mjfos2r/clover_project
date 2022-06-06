5f8de511009c2e2909c610f8044b921546bc7ab4
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-06 18:21:19 -0500

    first attempt at the batch script

--- fastqc/batchQC.sh
+++ fastqc/batchQC.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-#work in prog
-find *.fq.gz | xargs fastQC --outdir=/$OUTPUTDIR/$SAMPLEDIR/
+if [ $# -lt 2 ]   
+then                
+  echo "usage: batchQC <data> <report-dir>"
+  exit 1            
+fi 
+#set var to current directory so subshell doesn't mess up.
+WORKDIR=$PWD
+#subshell change to workdir
+cd $WORKDIR
+#lets set some variables
+DATADIR=$1
+REPORTDIR=$2
+#list a tree for the data directory
+tree $DATADIR
+#run treepeat.sh
+./treepeat.sh $DATADIR $REPORTDIR
+#run fastQC and output reports to the reportdirs.
+#if this works first time I'll buy a lotto ticket
+find *.fq.gz | xargs fastQC --output-dir $REPORTDIR 
