bf31672b8c90952fbef469bf8c121908b8ee8d0d
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-07 17:56:22 -0500

    broken

--- multiqc/batchQC.sh
+++ multiqc/batchQC.sh
@@ -21,12 +21,16 @@ tree $DATADIR
 # //todo 
 # //get the fastqc reports to be output into their corresponding directory depending on the input file.
 # // dunno how I'm gonna do that yet, may not be able to use find.
+echo "running treepeat.sh to copy the structure of $DATADIR into new directory $REPORTDIR"
 #run treepeat.sh
-#treepeat.sh $DATADIR $REPORTDIR
+treepeat.sh $DATADIR $REPORTDIR
 #run fastQC and output reports to the reportdirs.
 #if this works first time I'll buy a lotto ticket
-echo "creating $REPORTDIR in $WORKDIR"
-mkdir $REPORDIR/$DATADIR/
+#############
+# this is pointless I don't know Why I added it.
+#echo "creating $REPORTDIR in $WORKDIR"
+#mkdir $REPORDIR/$DATADIR/
+#############
 echo "looking for .fq.gz files in $DATADIR and outputting the fastQC report into $REPORTDIR"
 #can't wait for the addition of threads to completely break the script
 # I think i need to use sed to change the path output from find, changing the first $DATADIR/
