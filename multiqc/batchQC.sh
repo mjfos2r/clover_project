d7c2794d6f5b4c8b8b79447ceab228028277ecc7
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-07 17:41:01 -0500

    already know its busted

--- multiqc/batchQC.sh
+++ multiqc/batchQC.sh
@@ -4,12 +4,12 @@ then
   echo "usage: batchQC <data> <report-dir>"
   exit 1            
 fi 
+#* unnecessary
 #set var to current directory so subshell doesn't mess up.
 WORKDIR=$PWD
-
 #*
 # this broke the script
-# subshell change to workdir
+## subshell change to workdir
 #cd $WORKDIR
 #*
 
@@ -22,6 +22,10 @@ tree $DATADIR
 treepeat.sh $DATADIR $REPORTDIR
 #run fastQC and output reports to the reportdirs.
 #if this works first time I'll buy a lotto ticket
-find $DATADIR -name \*.fq.gz -print | xargs fastQC --outdir $REPORTDIR 
+echo "looking for .fq.gz files in $DATADIR and outputting the fastQC report into $REPORTDIR"
+#can't wait for the addition of threads to completely break the script
+# I think i need to use sed to change the path output from find, changing the first $DATADIR/
+# into $REPORTDIR/
+find -d -s $DATADIR -name \*.fq.gz -print | xargs fastQC --threads 60 --outdir $REPORTDIR/DATADIR/ 
 #okay lets try multiQC now
 multiqc $REPORTDIR
