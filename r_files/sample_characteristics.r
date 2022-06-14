e856f8f6b76b03b81aac0652d30cbe4750fcee07
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-14 10:36:37 -0500

    started notes for plotting pre assembly

---
+++ r_files/sample_characteristics.r
@@ -0,0 +1,10 @@
+#import xlsx file with sample characteristics
+#then import multiQC data
+#then plot various variables against each other
+#then cut adapters + filter
+#then align against reference transcriptome
+#pull gene expression quants from data
+#GO mapping?
+#network mapping? 
+#???
+#profit.
