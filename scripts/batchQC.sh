64396aadc8706776c8e6236aebd149ab1ec20352
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2022-06-30 11:03:56 -0500

    morning commit

--- scripts/batchQC.sh
+++ scripts/batchQC.sh
@@ -12,7 +12,7 @@ echo "Is Conda Activated?, better control C real quick if it isnt"
 #list a tree for the data directory
 tree $DATADIR
 echo "looking for .fq.gz files in $DATADIR and outputting the fastQC report into $REPORTDIR"
-find -d -s $DATADIR -name \*.fq.gz -print | xargs fastQC --threads 32 --outdir $REPORTDIR/$DATADIR/
+find -d -s $DATADIR -name \*.fq.gz -print | xargs fastQC --threads 32 --outdir $REPORTDIR
 #okay lets try multiQC now
 echo "running multiQC on reports in $REPORTDIR"
 echo "if conda is not activated and in the multiqc env, this won't work."
